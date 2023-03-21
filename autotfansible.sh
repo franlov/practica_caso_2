1ab50baf752164bd64a6417cdd7f1444fa7355b4
Francisco Leon <francisco.leong@gmail.com>
2023-03-21 11:18:17 +0100

    Fixes para maquinas que no tengan curl instalado

--- autotfansible.sh
+++ autotfansible.sh
@@ -39,7 +39,8 @@ echo ""
 if [ "$type" == "install" ];then
 echo "** Installing AZ CLI"
 echo ""
-sudo apt update && sudo apt install curl jq sed python3-pip
+sudo apt-get update
+sudo apt-get install libcurl4 curl jq sed python3-pip
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 echo ""
 echo "** Installing Terraform"
@@ -47,7 +48,7 @@ echo ""
 if [  -n "$(uname -a | grep Ubuntu)" ]; then
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform jq sed
+sudo apt-get update && sudo apt-get install terraform 
 else
 sudo yum install -y yum-utils curl jq sed python3-pip
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
